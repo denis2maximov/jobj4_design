a4d1589eb7cbcb193f155bd807ded4fcd1d19cdf
serioushippo work <denis2maksimov@gmail.com>
2023-06-27 16:46:26 +0300

    2.0. FileOutputStream [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,19 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+    public static void multiple(int size) {
+        try (FileOutputStream out = new FileOutputStream("data/dataresult.txt")) {
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    out.write((i + 1) * (j + 1));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
---
+++ src/test/java/ru/job4j/io/ResultFileTest.java
@@ -0,0 +1,50 @@
+package ru.job4j.io;
+
+import org.junit.internal.runners.statements.FailOnTimeout;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+class ResultFileTest {
+
+    @Test
+    public void when2on2() throws IOException {
+        ResultFile.multiple(2);
+        int[][] ex = new int[2][2];
+        try (FileInputStream input = new FileInputStream("data/dataresult.txt")) {
+            for (int i = 0; i < ex.length; i++) {
+                for (int j = 0; j < ex.length; j++) {
+                    ex[i][j] = input.read();
+                }
+            }
+        }
+        int[][] expect = {
+                {1, 2},
+                {2, 4}
+        };
+        assertThat(expect).isEqualTo(ex);
+    }
+
+    @Test
+    public void when3on3() throws IOException {
+        ResultFile.multiple(3);
+        int[][] ex = new int[3][3];
+        try (FileInputStream input = new FileInputStream("data/dataresult.txt")) {
+            for (int i = 0; i < ex.length; i++) {
+                for (int j = 0; j < ex.length; j++) {
+                    ex[i][j] = input.read();
+                }
+            }
+        }
+        int[][] expect = {
+                {1, 2, 3},
+                {2, 4, 6},
+                {3, 6, 9}
+        };
+        assertThat(expect).isEqualTo(ex);
+    }
+}
