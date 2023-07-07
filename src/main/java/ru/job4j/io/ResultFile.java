cd8d8c758695e2f3cee5b90e18b3752733527d0b
serious hippo <denis2maksimov@gmail.com>
2023-07-07 20:42:30 +0300

    2.0. FileOutputStream

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -8,7 +8,9 @@ public class ResultFile {
         try (FileOutputStream out = new FileOutputStream("data/dataresult.txt")) {
             for (int i = 0; i < size; i++) {
                 for (int j = 0; j < size; j++) {
-                  out.write(Integer.toString((i + 1) * (j + 1)).getBytes(StandardCharsets.UTF_8));
+                  out.write(Integer.toString((i + 1) * (j + 1))
+                          .getBytes(StandardCharsets.UTF_8));
+                  out.write((" ").getBytes());
                 }
                 out.write(System.lineSeparator().getBytes());
             }
@@ -18,8 +20,7 @@ public class ResultFile {
     }
 
     public static void main(String[] args) {
-        int size = 3;
-        multiple(size);
+        multiple(9);
     }
 }
 
---
+++ src/test/java/ru/job4j/assertj/SimpleConvertTest.java
@@ -0,0 +1,49 @@
+package ru.job4j.assertj;
+
+import org.assertj.core.data.Index;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.*;
+
+class SimpleConvertTest {
+
+    @Test
+    void toArray() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        String[] array = simpleConvert.toArray("first", "second", "three", "four", "five");
+        assertThat(array).hasSize(5)
+                .contains("second")
+                .contains("first", Index.atIndex(0))
+                .containsAnyOf("zero", "second", "six")
+                .doesNotContain("first", Index.atIndex(1));
+    }
+
+    @Test    void toList() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        List<String> list = simpleConvert.toList("first", "second", "three", "four", "five");
+          assertThat(list).hasSize(5)
+                .contains("five")
+                .anySatisfy(e -> {
+                    assertThat("first").isIn(list);
+                    assertThat(list).asString().containsPattern(".ou.");
+                    assertThat(list).last().isNotNull()
+                            .isEqualTo("five");
+                    })
+                  .allSatisfy(e -> {
+                      assertThat(e).isInstanceOf(String.class);
+                  });
+    };
+
+
+    @Test
+    void toSet() {
+    }
+
+    @Test
+    void toMap() {
+    }
+}
