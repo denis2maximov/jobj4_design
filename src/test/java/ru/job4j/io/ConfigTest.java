6e9c39191db82189910203477a12442dbbd53d77
serioushippo work <denis2maksimov@gmail.com>
2023-07-31 13:19:49 +0300

    5.2. BufferedOutputStream [#252490] - добавил тест на отсутсвующее значение в файле

--- src/test/java/ru/job4j/io/ConfigTest.java
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -2,28 +2,36 @@ package ru.job4j.io;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-
 import static org.assertj.core.api.Assertions.*;
 
 class ConfigTest {
     @Test
-    void whenPairWithoutComment() throws IOException {
+    void whenPairWithoutComment() {
         String path = "./data/pair_without_comment.properties";
         Config config = new Config(path);
         config.load();
         assertThat(config.value("name")).isEqualTo("Petr Arsentev");
     }
     @Test
-    void whenEmptyLines() throws IOException {
+    void whenEmptyLines() {
         String path = "./data/pair_with_comment.properties_and_empty_lines";
         Config config = new Config(path);
         config.load();
         assertThat(config.value("name")).isEqualTo("Petr Arsentev=1");
     }
     @Test
-    void whenPatternViolation()  {
+    void whenPatternViolationKey()  {
         String path = "./data/pair_with_pattern_violation";
+        Config config = new Config(path);
+        assertThatThrownBy(config::load)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("doesn't match the template!!");
+
+    }
+
+    @Test
+    void whenPatternViolationVolue()  {
+        String path = "./data/pair_with_pattern_violation_volume";
         Config config = new Config(path);
         assertThatThrownBy(config::load)
                 .isInstanceOf(IllegalArgumentException.class)
