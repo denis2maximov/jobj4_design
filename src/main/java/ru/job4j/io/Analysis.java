63220908295b925d702d5b7d6757920d36b81be8
serious hippo <denis2maksimov@gmail.com>
2023-08-05 21:49:14 +0300

    7. Анализ доступности сервера - переделал

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -1,23 +1,32 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.*;
 
 public class Analysis {
     public void unavailable(String source, String target) throws IOException {
         try (BufferedReader reader = new BufferedReader(new FileReader(source));
              BufferedWriter writer = new BufferedWriter(new FileWriter(target))) {
-            List<String> list = new ArrayList<>();
-            reader.lines().filter(s -> !s.startsWith("200"))
-                    .map(s -> s.split(" "))
-                    .map(s -> s[1])
-                    .forEach(list::add);
-            writer.write(list.get(0) + " ; " + list.get(list.size() - 1));
+            boolean work = true;
+            String string;
+            while ((string = reader.readLine()) != null) {
+                String[] arrayString = string.split(" ");
+                if ((arrayString[0].equals("400") ||  arrayString[0].equals("500")) && work) {
+                    writer.append(arrayString[1]).append(";");
+                    work = false;
+                }
+
+                if ((arrayString[0].equals("200") || arrayString[0].equals("300")) && !work) {
+                    writer.append(arrayString[1]).append(";").append(System.lineSeparator());
+                    work = true;
+                }
+            }
         }
-    }
+            }
+
 
     public static void main(String[] args) throws IOException {
         Analysis analysis = new Analysis();
         analysis.unavailable("data/server.log", "data/target.csv");
+        analysis.unavailable("data/server1.log", "data/target1.csv");
     }
 }
