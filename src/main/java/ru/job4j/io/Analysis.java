d785b2d4122d4074b075dfa3720ab47a5a859d6a
serious hippo <denis2maksimov@gmail.com>
2023-08-06 18:22:56 +0300

    7. Анализ доступности сервера - переделал с одним if

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -1,32 +1,39 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.text.Format;
+import java.util.Arrays;
+import java.util.function.Predicate;
 
 public class Analysis {
+
     public void unavailable(String source, String target) throws IOException {
         try (BufferedReader reader = new BufferedReader(new FileReader(source));
              BufferedWriter writer = new BufferedWriter(new FileWriter(target))) {
             boolean work = true;
             String string;
+            StringBuilder stringBuilder = new StringBuilder();
             while ((string = reader.readLine()) != null) {
                 String[] arrayString = string.split(" ");
-                if ((arrayString[0].equals("400") ||  arrayString[0].equals("500")) && work) {
-                    writer.append(arrayString[1]).append(";");
-                    work = false;
+                if ((((arrayString[0].equals("400")
+                        || arrayString[0].equals("500")) && work) || (arrayString[0].equals("200")
+                        || arrayString[0].equals("300")) && !work)) {
+                    stringBuilder.append(arrayString[1]).append(";");
+                   work = !work;
                 }
-
-                if ((arrayString[0].equals("200") || arrayString[0].equals("300")) && !work) {
-                    writer.append(arrayString[1]).append(";").append(System.lineSeparator());
-                    work = true;
-                }
+            }
+                String[] stringOut = stringBuilder.toString().split(";");
+            for (int i = 0; i < stringOut.length - 1; i = i + 2) {
+                writer.append(stringOut[i]).append(";").append(stringOut[i + 1]).append(";");
+                writer.write(System.lineSeparator());
+            }
             }
         }
+
+            public static void main(String[]args) throws IOException {
+                Analysis analysis = new Analysis();
+                analysis.unavailable("data/server.log", "data/target.csv");
+                analysis.unavailable("data/server1.log", "data/target1.csv");
             }
+        }
 
-
-    public static void main(String[] args) throws IOException {
-        Analysis analysis = new Analysis();
-        analysis.unavailable("data/server.log", "data/target.csv");
-        analysis.unavailable("data/server1.log", "data/target1.csv");
-    }
-}
