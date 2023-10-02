0c48cb85d9ef100bf15d0c8e7eb687514a82135d
serioushippo <denis2maksimov@gmail.com>
2023-10-02 22:36:04 +0300

    11. Поиск дубликатов [#315066 #449538]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -9,43 +9,46 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.groupingBy;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-    private HashMap<Path, FileProperty> map =  new HashMap<>();
+    private static Map<Path, FileProperty> map = new HashMap<>();
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-       FileProperty fileProperty = new FileProperty(Files.size(file), file.getFileName().toString());
-
-       if (attrs.isRegularFile()) {
-           map.put(file.toAbsolutePath(), fileProperty);
-       }
-
-        Map<FileProperty, List<Path>> rsl = map.keySet().stream()
-                .collect(groupingBy(map::get));
-
-                 rsl.values().stream()
-                                .filter(v -> v.size() > 1)
-                         .forEach((v) -> {
-                                     for (Map.Entry<FileProperty, List<Path>> pair : rsl.entrySet()) {
-                                         if (v.equals(pair.getValue())) {
-                                             System.out.println("Name of the duplicate: "
-                                                     + pair.getKey().getName()
-                                                     + "Size, bytes: "
-                                                     + pair.getKey().getSize());
-                                             List<Path> list = pair.getValue();
-                                             for (Path path : list) {
-                                                 System.out.println("-- " + path.toString());
-                                             }
-                                             System.out.println();
-                                         }
-                                     }
-                                 });
-
+        FileProperty fileProperty = new FileProperty(Files.size(file.toAbsolutePath()), file.getFileName().toString());
+        map.put(file.toAbsolutePath(), fileProperty);
         return super.visitFile(file, attrs);
-     }
     }
+
+    public static Map<FileProperty, List<Path>> filter() {
+        Map<FileProperty, List<Path>> map2 = map.keySet().stream()
+                .collect(groupingBy(map::get));
+        return map2.entrySet().stream()
+                .filter(v -> v.getValue().size() > 1)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+    public static void print() {
+        DuplicatesVisitor.filter().values().stream()
+                .filter(v -> v.size() > 1)
+                .forEach((v) -> {
+                    for (Map.Entry<FileProperty, List<Path>> pair : DuplicatesVisitor.filter().entrySet()) {
+                        if (v.equals(pair.getValue())) {
+                            System.out.println("Name of the duplicate: "
+                                    + pair.getKey().getName()
+                                    + " Size, bytes: "
+                                    + pair.getKey().getSize());
+                            List<Path> list = pair.getValue();
+                            for (Path path : list) {
+                                System.out.println("-- " + path.toString());
+                            }
+                            System.out.println();
+                        }
+                    }
+                });
+            }
+  }
 
 
 
